7c56d384707f9a8e9c43271e603d92af47b5b51a
DESKTOP-8M7P8QQ\micha <mmborland@gmail.com>
2017-01-31 09:28:10 -0600

    added the new stuff for the 4th lab.

--- src/edu/simpson/michael/borland/assignments/NameListGet.java
+++ src/edu/simpson/michael/borland/assignments/NameListGet.java
@@ -48,6 +48,7 @@ public class NameListGet extends javax.servlet.http.HttpServlet {
         }
         out.println("]\n");
 
+
         }
 
 
---
+++ web/name_list.js
@@ -0,0 +1,3 @@
+/**
+ * Created by micha on 1/31/2017.
+ */
