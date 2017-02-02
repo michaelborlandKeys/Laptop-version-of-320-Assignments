c382d84981ad32f36848d8ee7f85e9b0072ce704
DESKTOP-8M7P8QQ\micha <mmborland@gmail.com>
2017-02-02 12:26:46 -0600

    Assignment finished and working locally, but it is not working on the server

--- web/name_list.js
+++ web/name_list.js
@@ -1,3 +1,46 @@
 /**
  * Created by micha on 1/31/2017.
  */
+
+
+
+
+function displayTableRecords() {
+    // Here's where your code is going to go.
+
+
+    var url = "api/name_list_get";
+
+    $.getJSON(url, null, function(display_Json_Data) {
+        // display_Json_Data is an object. You can set a breakpoint, or print
+        // it to see the fields. Specifically, it is an array of objects.
+        // Here we loop the array and print the first name.
+
+
+        $("#displayUserInfo")[0].rows[1].remove();
+        for (var i = 0; i < display_Json_Data.length; i++) {
+            var fieldDataGet = $('#displayUserInfo').val();
+            var fieldDataDisplay = "<tr><td>"+display_Json_Data[i].id+"</td><td>"+display_Json_Data[i].first+"</td><td>"+display_Json_Data[i].last+"</td>" +
+                                    "<td>"+display_Json_Data[i].phone+"</td><td>"+display_Json_Data[i].email+"</td><td>"+display_Json_Data[i].birthday+"</td></tr>";
+            $("#displayUserInfo tbody").append(fieldDataDisplay);
+            console.log("testing if the log prints to the console on server")
+
+            console.log(display_Json_Data[i].id);
+            console.log(display_Json_Data[i].first);
+            console.log(display_Json_Data[i].last);
+            console.log(display_Json_Data[i].phone);
+            console.log(display_Json_Data[i].email);
+            console.log(display_Json_Data[i].birthday);
+
+        }
+
+    }
+);
+
+}
+
+
+// Call your code.
+displayTableRecords();
+
+
